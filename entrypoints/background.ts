31c1c073f572f6ba0cab341f95840b3c169bae2c
woody <mat.g.890517@gmail.com>
2026-08-26 15:03:38 +0000

    Disable the toolbar icon off jobcakes.com.
    Left-click opens the side panel only on Jobcakes. Other sites grey the icon so Chrome does not show the site-access menu.

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,5 +1,43 @@
 export default defineBackground(() => {
   console.log('Hello background!', { id: browser.runtime.id });
-  browser.sidePanel?.setPanelBehavior({ openPanelOnActionClick: true });
+  browser.sidePanel?.setPanelBehavior({ openPanelOnActionClick: false });
+
+  const TARGETS = [/jobcakes\.com$/i];
+
+  function isTarget(url?: string) {
+    if (!url) return false;
+    try {
+      return TARGETS.some((re) => re.test(new URL(url).hostname));
+    } catch {
+      return false;
+    }
+  }
+
+  async function syncTab(tabId: number, url?: string) {
+    const on = isTarget(url);
+    await browser.action[on ? 'enable' : 'disable'](tabId);
+    await browser.sidePanel?.setOptions({ tabId, enabled: on });
+  }
+
+  browser.tabs.onUpdated.addListener((tabId, info, tab) => {
+    if (info.url || info.status === 'complete') syncTab(tabId, info.url ?? tab.url);
+  });
+
+  browser.tabs.onActivated.addListener(async ({ tabId }) => {
+    const tab = await browser.tabs.get(tabId);
+    syncTab(tabId, tab.url);
+  });
+
+  browser.action.onClicked.addListener(async (tab) => {
+    if (tab.id != null && isTarget(tab.url)) {
+      await browser.sidePanel.open({ tabId: tab.id });
+    }
+  });
+
+  browser.tabs.query({}).then((tabs) => {
+    for (const tab of tabs) {
+      if (tab.id != null) syncTab(tab.id, tab.url);
+    }
+  });
   
 });
